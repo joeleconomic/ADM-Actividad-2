433e17cada6814912a75acc9d3890cc50535684e
alejandrobm-max <alejandro.bejarano901@gmail.com>
2026-01-15 16:31:38 +0100

    Script.R

--- Script.R
+++ Script.R
@@ -1,5 +1,5 @@
 
-# 1. Descripción de la tarea ----
+# 1 Descripción de la tarea ----
 
 # Toma de contacto con los datos, limpieza, transformación y análisis exploratorio inicial
 
@@ -19,7 +19,7 @@
 # Lo usaremos al final de la actividad como forma de VALIDACIÓN DEL MODELO
 
 
-# 2. Carga y transformación de datos ----
+# 2 Carga y transformación de datos ----
 ## 2.1 Librerias necesarias ----
 library(tidyverse)    # Para tratamiento y visualización de datos
 library(broom)        # Para tidy summaries
@@ -86,6 +86,81 @@ prop.table(table(data2$Abandono))
 # 3 Modelización ----
 
 ## 3.1 Modelo Logit ----
+m1 <- glm(Abandono ~ Contrato + Factura_digital + Servicio_Internet +
+            Soporte_tecnico + CopiaSeguridad_Online + Television_carta +
+            Meses_alta, 
+          family = "binomial",
+          data = data2)
+
+summary(m1)  ## ¿Cómo se podría obtener una clasificación de la importancia de las variables en nuestro modelo? 
+## Analizamos en el informe los Signif. codes de cada variable
+
+# A) Odd Ratios: Para poder interpretar estos β (Coeficientes estimados)
+OR <- exp(cbind(coef(m1), confint(m1)))
+print("Odds Ratios (IC 95%):")
+print(OR)
+
+# B) Efecto Marginal Promedio (AME): Cómo cambia la probabilidad de Abandono,
+# cuando otra variable cambia, en el caso de las Dummies, se evalua con la 
+# categoría de referencia.
+margins(m1)
+
+# C) Predicción del modelo LOGIT
+m1pred <- predict(m1, type = "response")
+
+# Se puede observar que hemos hecho una predicción para cada individuo,
+# pero, nuestras predicciones se encuentran entre 0 y 1 -> Hay que codificarlas
+View(m1pred)
+
+# D) Predicción del modelo LOGIT haciendo la codificación a "Yes" / "No",
+# además fijamos el umbral en 0.5
+m1predcod <- ifelse(m1pred > 0.5, "Yes", "No") |> 
+  factor(levels = c("No", "Yes"))
+
+# Matriz de confusión
+confusionMatrix(m1predcod, data2$Abandono)
+
+### El modelo ha acertado un 79.22% de las veces ###
+
+# Probaremos a cambiar el umbral a 0.65, 
+# para comprobar si esto mejora o empeora el modelo
+m1predcod1 <- ifelse(m1pred > 0.65, "Yes", "No") |> 
+  factor(levels = c("No", "Yes"))
+
+# Matriz de confusión
+confusionMatrix(m1predcod1, data2$Abandono)
+
+# Ahora, el modelo acertó un 78.28% de las veces, por lo que EMPEORÓ el modelo
+
+### NOS QUEDAREMOS CON EL UMBRAL FIJADO EN 0.5 ### 
+## QUE ACIERTA EN UN 79.22% DE LAS VECES ###
+
+
+## 3.2 Modelo Arbol de decisión ---- 
+
+m2 <- rpart(Abandono ~ Contrato + Factura_digital + Servicio_Internet +
+            Soporte_tecnico + CopiaSeguridad_Online + Television_carta +
+            Meses_alta,
+            method = "class",
+            data = data2)
+
+summary(m2)
+
+rpart.plot(m2, tweak= 1)
+
+rpart.plot(m2, tweak=1, branch.col = "grey40", branch.lwd = 2,
+           compress = TRUE, uniform = TRUE)
+
+
+## 4 ¿Qué modelo de clasificación tiene una mayor precisión? ----
+
+
+## 5 Partición del conjunto de datos: validación del modelo ----
+
+##### DEJO ESTO POR AQUÍ POR SI FUERA ÚTIL PARA LA PARTICIÓN CON LA QUE SE VALIDA EL MODELO:
+# (Era lo que inicialmente estaba en el Modelo Logit de Joel, que incluía una partición)
+
+## Modelo Logit ----
 
 m1 <- glm(Abandono ~ Contrato + Factura_digital + Servicio_Internet +
           Soporte_tecnico + CopiaSeguridad_Online + Television_carta +
@@ -117,18 +192,3 @@ pred_test <- ifelse(prob_test > 0.5, "Yes", "No") |>
 confusionMatrix(pred_test, data_test$Abandono)
 
 # 79.29% de las observaciones se clasificaron correctamente
-
-## 3.2 Modelo Arbol de decisión ----
-
-m2 <- rpart(Abandono ~ Contrato + Factura_digital + Servicio_Internet +
-            Soporte_tecnico + CopiaSeguridad_Online + Television_carta +
-            Meses_alta,
-            method = "class",
-            data = data2)
-
-summary(m2)
-
-rpart.plot(m2, tweak= 1)
-
-rpart.plot(m2, tweak=1, branch.col = "grey40", branch.lwd = 2,
-           compress = TRUE, uniform = TRUE)
